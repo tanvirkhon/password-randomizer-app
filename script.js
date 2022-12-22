20c3fa8128aa69201c42015b7516ab0b1d8029c2
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2022-12-22 17:14:28 -0600

    Added check password function to js file

--- script.js
+++ script.js
@@ -15,6 +15,50 @@ function createRandomPassword(Length, string) {
     "Your new password is: " + randomString;
 }
 
+/* -----------------------------------------------------
+Check Password Function
+----------------------------------------------------- */
+
+function checkPasswordCriteria() {
+  //These 4 prompts will capture what kind of password the user wants and put them into a variable
+  var upperCase = window.confirm(
+    "Do you want uppercase letters in your password?"
+  );
+  var loweCase = window.confirm(
+    "Do you want lowercase letters in your password?"
+  );
+  var numericChar = window.confirm("Do you want numbers in your password?");
+  var specialChar = window.confirm(
+    "Do you want special characters in your password?"
+  );
+
+  var password = "";
+  var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericCharacters = "0123456789";
+  var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  //These if statements will check to see if the user confirmed and option. If they did the password Variable will include that string of characters.
+
+  if (upperCase) {
+    password += upperCaseLetters;
+  }
+
+  if (loweCase) {
+    password += lowerCaseLetters;
+  }
+
+  if (numericChar) {
+    password += numericCharacters;
+  }
+
+  if (specialChar) {
+    password += specialCharacters;
+  }
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
